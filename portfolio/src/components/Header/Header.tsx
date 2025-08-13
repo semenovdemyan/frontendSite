9895108602170b2d9bd3f8fb7a381d915920a672
Demian Semenov <semenovdemyan@gmail.com>
2025-08-13 10:27:06 +0300

    update

--- portfolio/src/components/Header/Header.tsx
+++ portfolio/src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 'use client'
 import { FC } from 'react'
 import { Button } from '../Button/Button'
+import './Header.module.scss'
 
 export const Header: FC = () => {
   return (
     <>
       <header>
-        <nav>
+        <nav className="nav">
           <Button label="ME" repeatLabelCount={9} href="/about" />
           <Button label="MY WORKS" repeatLabelCount={9} href="/works" />
           <Button label="MY SKILLS" repeatLabelCount={9} href="/skills" />
