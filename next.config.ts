59939e9a923edcb2427e6c6c3e91385973225caa
Demian Semenov <semenovdemyan@gmail.com>
2026-08-03 01:01:19 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
-import type { NextConfig } from 'next'
+const nextConfig = {
 
-const nextConfig: NextConfig = {
-  // НИЧЕГО не добавляйте сюда!
-  // Next.js 15 сам настроит SCSS модули
 }
 
 export default nextConfig
